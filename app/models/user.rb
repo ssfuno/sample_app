6e6ed126e2e03f2a657e886c5c533727de727c5d
ssfuno <96967819+ssfuno@users.noreply.github.com>
2023-05-30 23:20:07 +0900

    create controller with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :registerable
   validates :email, :name, :password, :password_confirmation, presence: true
   has_many :recipes
   has_many :favorites
+
+  validate :password_regex
+
+  private
+
+  def password_regex
+    return if password.blank? || password =~ /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+
+    errors.add :password, 'は半角英数字を1種類ずつ含み6文字以上としてください。'
+  end
 end
