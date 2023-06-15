9eb88059229f4abb0adaa2f247bb7227bc998aa9
ssfuno <96967819+ssfuno@users.noreply.github.com>
2023-06-15 09:36:50 +0900

    fix favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,17 +7,28 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.find_by(id: params[:recipe_id])
     # 自分のレシピはお気に入りに登録できないようにする
-    return if Recipe.find_by(id: params[:recipe_id], user_id: current_user.id)
-    favorite = current_user.favorites.build(recipe_id: params[:recipe_id])
+    return if Recipe.find_by(id: @recipe.id, user_id: current_user.id)
+    
+    favorite = current_user.favorites.build(recipe_id: @recipe.id)
     favorite.save
-    # redirect_to recipe_path(params[:recipe_id])
+    render turbo_stream: turbo_stream.replace(
+      'favorite',
+      partial: 'favorites/favorite',
+      locals: { recipe: @recipe, liked: true},
+    )
   end
   
   def destroy
-    favorite = Favorite.find_by(recipe_id: params[:recipe_id], user_id: current_user.id)
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    favorite = Favorite.find_by(recipe_id: @recipe.id, user_id: current_user.id)
     favorite.destroy
-    # redirect_to recipe_path(params[:recipe_id])
+    render turbo_stream: turbo_stream.replace(
+      'favorite',
+      partial: 'favorites/favorite',
+      locals: { recipe: @recipe, liked: false },
+    )
   end
 
   private
